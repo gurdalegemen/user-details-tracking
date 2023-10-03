f49fc63071299ac3533e8b5426030c1515c9cf12
gurdalegemen <gurdalegemen@gmail.com>
2023-10-03 13:13:46 +0300

    install tailwindcss

--- details-tracking/src/App.vue
+++ details-tracking/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
+  import ExComp from './components/ExampleComponent.vue'
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    
+    <ExComp/>
   </main>
 </template>
 
--- details-tracking/src/components/ExampleComponent.vue
+++ details-tracking/src/components/ExampleComponent.vue
@@ -1,21 +1,16 @@
 <script setup>
-// defineProps({
-//   msg: {
-//     type: String,
-//     required: true
-//   }
-// })
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <template>
-  <!-- <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div> -->
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped>
