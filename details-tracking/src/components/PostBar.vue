89f2e4fc8d737da13ec797daea6942b30039bb04
gurdalegemen <gurdalegemen@gmail.com>
2023-10-06 00:52:39 +0300

    PostBar added

---
+++ details-tracking/src/components/PostBar.vue
@@ -0,0 +1,20 @@
+<script>
+   export default {
+    props: {
+        post: Object,
+    },
+   
+   }
+
+</script>
+
+<template>
+    <!-- <div class="h-12 flex items-center pl-2">
+        <input type="checkbox" class="w-4 h-4 accent-primary" v-model="post.completed">
+        <p class="pl-5 text-sm capitalize">{{post.title}}</p>
+    </div> -->
+</template>
+
+<style scoped>
+    
+</style>
